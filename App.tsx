35cf37a62085e866de1579bfc49b0b04f260708a
charanjuluru1 <charanjuluru1@gmail.com>
2021-09-15 03:47:18 +0530

    first commit

---
+++ App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { createAppContainer } from 'react-navigation';
+
+
+
+
+const App: () => JSX.Element = () => {
+  return (
+    <>
+
+    </>
+  );
+};
+
+export default App;
---
+++ template.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  placeholderName: 'myPreemieRN',
+  templateDir: './template',
+}
